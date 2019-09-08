8e756921b73270b99140d2b50c749a20afdb13ec
Mayank Devrani <48404161+mayankdevrani@users.noreply.github.com>
2019-09-08 21:38:08 +0530

    Updates

---
+++ precache-manifest.4e7ab16abb397fa833fc0c216dc07801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae0ba8ab4070bb78405323de22c4010",
+    "url": "/mayankdevrani/React-ToDO-.git/index.html"
+  },
+  {
+    "revision": "8879e093186753f24e58",
+    "url": "/mayankdevrani/React-ToDO-.git/static/css/main.07e14ca4.chunk.css"
+  },
+  {
+    "revision": "b12899ed8f2e6ed297fe",
+    "url": "/mayankdevrani/React-ToDO-.git/static/js/2.abdf2c10.chunk.js"
+  },
+  {
+    "revision": "8879e093186753f24e58",
+    "url": "/mayankdevrani/React-ToDO-.git/static/js/main.35fbeb79.chunk.js"
+  },
+  {
+    "revision": "ca0244ac822d7e8d2e06",
+    "url": "/mayankdevrani/React-ToDO-.git/static/js/runtime~main.0b7a1869.js"
+  }
+]);
